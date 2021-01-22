7bbb2cdc7c2a0806d5762902808e53057ec4c469
diegoririhena <dririhena3@gmail.com>
2021-01-22 15:18:32 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, eius dicta, et earum id ipsam, facere assumenda aspernatur suscipit sequi alias porro nulla. Optio veritatis ut itaque. Veritatis, distinctio itaque.</p>
 </body>
 </html>
